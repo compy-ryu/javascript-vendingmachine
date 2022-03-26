105d433e220d02f8ca65a55bceceaa583e53384b
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-03-26 15:10:00 +0900

    Updates

--- bundle.js
+++ bundle.js
@@ -146,6 +146,18 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _Utils_VendingMachine_validator__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @Utils/VendingMachine/validator */ "./src/es/utils/VendingMachine/validator.ts");
 /* harmony import */ var _Store_HoldingAmountStore__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @Store/HoldingAmountStore */ "./src/es/Store/HoldingAmountStore.ts");
 /* harmony import */ var _Display_template__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @Display/template */ "./src/es/display/template.js");
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && iter[Symbol.iterator] != null || iter["@@iterator"] != null) return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
@@ -191,10 +203,14 @@ var HoldingAmountPage = /*#__PURE__*/function () {
     _defineProperty(this, "render", function (_ref) {
       var state = _ref.state,
           changeStates = _ref.changeStates;
+      var renderTargetMethod = new Set();
       changeStates.forEach(function (stateKey) {
-        _this.renderMethodList[stateKey].forEach(function (renderMethod) {
-          return renderMethod(state);
-        });
+        renderTargetMethod.add.apply(renderTargetMethod, _toConsumableArray(_this.renderMethodList[stateKey].map(function (renderMethod) {
+          return renderMethod;
+        })));
+      });
+      renderTargetMethod.forEach(function (renderMethod) {
+        return renderMethod(state);
       });
     });
 
@@ -272,6 +288,18 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _Utils_VendingMachine_validator__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @Utils/VendingMachine/validator */ "./src/es/utils/VendingMachine/validator.ts");
 /* harmony import */ var _Store_ProductStore__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @Store/ProductStore */ "./src/es/Store/ProductStore.ts");
 /* harmony import */ var _Display_template__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @Display/template */ "./src/es/display/template.js");
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && iter[Symbol.iterator] != null || iter["@@iterator"] != null) return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
@@ -317,10 +345,14 @@ var ProductPage = /*#__PURE__*/function () {
     _defineProperty(this, "render", function (_ref) {
       var state = _ref.state,
           changeStates = _ref.changeStates;
+      var renderTargetMethod = new Set();
       changeStates.forEach(function (stateKey) {
-        _this.renderMethodList[stateKey].forEach(function (renderMethod) {
-          return renderMethod(state);
-        });
+        renderTargetMethod.add.apply(renderTargetMethod, _toConsumableArray(_this.renderMethodList[stateKey].map(function (renderMethod) {
+          return renderMethod;
+        })));
+      });
+      renderTargetMethod.forEach(function (renderMethod) {
+        return renderMethod(state);
       });
     });
 
